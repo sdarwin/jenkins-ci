3c5948744da32c295c992906e3247dea730c26ab
Sam Darwin <samuel.d.darwin@gmail.com>
2024-11-15 15:50:56 -0700

    http_io (#7)

---
+++ scripts/cppalliance_http_io_prebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+echo "Using sudo"
+echo "export PRTEST=prtest2" >> jenkinsjobinfo.sh
+# Added to container
+# sudo apt-get update
+# sudo apt-get install -y openssl libssl-dev
+
+# How to make this more durable for future clang?
+# Or, URL has already been updated.
+echo "export CXX=/usr/bin/clang++-18" >> jenkinsjobinfo.sh
+echo "export CC=/usr/bin/clang-18" >> jenkinsjobinfo.sh
