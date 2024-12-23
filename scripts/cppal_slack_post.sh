0ea8b82d10e893e1ad230b87de85378702065f51
sdarwin <samuel.d.darwin@gmail.com>
2024-12-23 20:12:58 +0000

    ccc

--- scripts/cppal_slack_post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -xe
-PREVIEWMESSAGE="A preview of the cppalliance website is available at https://${CHANGE_ID}.${DNSREPONAME}.prtest.cppalliance.org"
-curl -X POST -H 'Content-type: application/json' --data  "{\"text\":\"$PREVIEWMESSAGE\"}"  ${CPPAL_SLACK_WEBHOOK}
