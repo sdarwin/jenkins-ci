5ca572524edd754b09a8964f383f05af4e2419f9
sdarwin <samuel.d.darwin@gmail.com>
2025-08-25 15:45:43 -0600

    ws_io pre-reqs

--- scripts/cppalliance_ws_io_prebuild.sh
+++ scripts/cppalliance_ws_io_prebuild.sh
@@ -2,4 +2,4 @@
 
 set -xe
 echo "export PRTEST=prtest3" >> jenkinsjobinfo.sh
-echo "export EXTRA_BOOST_LIBRARIES='cppalliance/buffers cppalliance/http_proto cppalliance/http_io cppalliance/ws_proto'" >> jenkinsjobinfo.sh
+echo "export EXTRA_BOOST_LIBRARIES='cppalliance/buffers cppalliance/http_io cppalliance/http_proto cppalliance/rts cppalliance/ws_proto'" >> jenkinsjobinfo.sh
