cc141b499daedbb9e1181589e469350ebcfa7647
sdarwin <samuel.d.darwin@gmail.com>
2025-11-13 15:46:33 +0000

    beast2_lcov

---
+++ scripts/cppalliance_beast2_lcov_prebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+echo "Using sudo"
+echo "export PRTEST=prtest3" >> jenkinsjobinfo.sh
+# Added to container
+# sudo apt-get update
+# sudo apt-get install -y openssl libssl-dev
+
+# How to make this more durable for future clang?
+# Or, URL has already been updated.
+echo "export CXX=/usr/bin/clang++-18" >> jenkinsjobinfo.sh
+echo "export CC=/usr/bin/clang-18" >> jenkinsjobinfo.sh
+# buffers may not be required
+echo "export EXTRA_BOOST_LIBRARIES='cppalliance/buffers cppalliance/http_proto cppalliance/rts'" >> jenkinsjobinfo.sh
+
