d88ae8dee8688389decfed4793ea269f38fd902c
sdarwin <samuel.d.darwin@gmail.com>
2026-06-05 14:30:25 +0000

    cppalliance/burl

--- job-setup/create-jenkins-job.sh
+++ job-setup/create-jenkins-job.sh
@@ -340,7 +340,6 @@ prebuild_script=$(cat <<EOF
 
 set -xe
 echo "export PRTEST=prtest3" >> jenkinsjobinfo.sh
-echo "export ONLY_BUILD_ON_DOCS_MODIFICATION=true" >> jenkinsjobinfo.sh
 echo "export EXTRA_BOOST_LIBRARIES='cppalliance/buffers cppalliance/capy cppalliance/corosio cppalliance/http'" >> jenkinsjobinfo.sh
 
 EOF
